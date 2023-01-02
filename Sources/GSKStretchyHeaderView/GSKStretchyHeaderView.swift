2bb5b47122424bf8b41ed44637d6321fb38a50f0
Jakub Gencer <jakub.gencer@allaboutapps.at>
2023-01-02 14:53:02 +0100

    Added SPM support

--- Sources/GSKStretchyHeaderView/GSKStretchyHeaderView.swift
+++
@@ -1,6 +0,0 @@
-public struct GSKStretchyHeaderView {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- Tests/GSKStretchyHeaderViewTests/GSKStretchyHeaderViewTests.swift
+++ Tests/GSKStretchyHeaderViewTests/GSKStretchyHeaderViewTests.swift
@@ -1,11 +1,4 @@
 import XCTest
 @testable import GSKStretchyHeaderView
 
-final class GSKStretchyHeaderViewTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(GSKStretchyHeaderView().text, "Hello, World!")
-    }
-}
+final class GSKStretchyHeaderViewTests: XCTestCase {}
